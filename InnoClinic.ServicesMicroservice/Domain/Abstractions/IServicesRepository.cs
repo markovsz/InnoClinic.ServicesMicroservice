42832fda5ff929b3b2c15809337ad55585206be8
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-16 14:47:09 +0300

    add repository abstractions

---
+++ InnoClinic.ServicesMicroservice/Domain/Abstractions/IServicesRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.RequestParameters;
+
+namespace Domain.Abstractions;
+
+public interface IServicesRepository
+{
+    Task<Guid> CreateAsync(Service service);
+    Task ChangeStatusAsync(Guid id, string status);
+    Task<Service> GetByIdAsync(Guid id);
+    Task<Service> GetByNameAsync(string name);
+    Task<bool> Exists(Guid id);
+    Task<IEnumerable<Service>> GetAsync(ServiceParameters parameters);
+    Task UpdateAsync(Service service);
+    Task LinkWithSpecializationByIdAsync(Guid id, Guid specializationId);
+}
---
+++ InnoClinic.ServicesMicroservice/Domain/Abstractions/ISpecializationsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions;
+
+public interface ISpecializationsRepository
+{
+    Task<Guid> CreateAsync(Specialization specialization);
+    Task ChangeStatusAsync(Guid id, string status);
+    Task<Specialization> GetByIdAsync(Guid id);
+    Task<Specialization> GetByNameAsync(string name);
+    Task<bool> Exists(Guid id);
+    Task<IEnumerable<Specialization>> GetAsync();
+    Task UpdateAsync(Specialization specialization);
+}
