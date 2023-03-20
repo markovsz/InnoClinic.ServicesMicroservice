c91348ef48dd92345b8b150f4d222d2f72959282
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-20 17:23:31 +0300

    add service abstractions

---
+++ InnoClinic.ServicesMicroservice/Application/Abstractions/IServiceCategoriesService.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Abstractions;
+
+public interface IServiceCategoriesService
+{
+    Task<Service> GetByIdAsync(Guid id);
+    Task<IEnumerable<Service>> GetAsync();
+}
