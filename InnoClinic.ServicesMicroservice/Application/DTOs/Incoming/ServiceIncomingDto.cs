b2b437171d97971651dba48b4aee5c35eb436054
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-20 17:23:42 +0300

    add DTOs

---
+++ InnoClinic.ServicesMicroservice/Application/DTOs/Incoming/ServiceIncomingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs.Incoming;
+
+public class ServiceIncomingDto
+{
+    public string Name { get; set; }
+    public Decimal Price { get; set; }
+    public Guid CategoryId { get; set; }
+    public Guid SpecializationId { get; set; }
+    public string Status { get; set; }
+}
