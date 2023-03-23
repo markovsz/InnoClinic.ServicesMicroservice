7f6f06759e68a3780ade4694b76104371e12c63b
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-23 15:01:16 +0300

    add dependencies

--- InnoClinic.ServicesMicroservice/Api/Program.cs
+++ InnoClinic.ServicesMicroservice/Api/Program.cs
@@ -1,6 +1,13 @@
+using Api.Extensions;
+using Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureDb();
+builder.Services.ConfigureRepositories();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.ConfigureServices();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
