1d7d589164f8b7069117edbf8eb05f6bc74dd116
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-23 14:58:48 +0300

    add controllers

---
+++ InnoClinic.ServicesMicroservice/Api/Controllers/ServicesController.cs
@@ -0,0 +1,54 @@
+﻿using Application.Abstractions;
+using Application.DTOs.Incoming;
+using Domain.RequestParameters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly IServicesService _servicesService;
+
+        public ServicesController(IServicesService servicesService)
+        {
+            _servicesService = servicesService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateServiceAsync(ServiceIncomingDto service)
+        {
+            var id = await _servicesService.CreateAsync(service);
+            return CreatedAtRoute("GetServiceById", new { id = id }, id);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetServicesAsync([FromQuery] ServiceParameters parameters)
+        {
+            var services = await _servicesService.GetAsync(parameters);
+            return Ok(services);
+        }
+
+        [HttpGet("service/{id}", Name = "GetServiceById")]
+        public async Task<IActionResult> GetServiceByIdAsync(Guid id)
+        {
+            var service = await _servicesService.GetByIdAsync(id);
+            return Ok(service);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateServiceAsync(Guid id, ServiceIncomingDto incomingDto)
+        {
+            await _servicesService.UpdateAsync(id, incomingDto);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/status")]
+        public async Task<IActionResult> ChangeServiceStatusAsync(Guid id, [FromBody] string status)
+        {
+            await _servicesService.ChangeStatusAsync(id, status);
+            return NoContent();
+        }
+    }
+}
---
+++ InnoClinic.ServicesMicroservice/Api/Controllers/SpecializationsController.cs
@@ -0,0 +1,53 @@
+﻿using Application.Abstractions;
+using Application.DTOs.Incoming;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecializationsController : ControllerBase
+    {
+        private ISpecializationsService _specializationsService;
+
+        public SpecializationsController(ISpecializationsService specializationsService) 
+        {
+            _specializationsService = specializationsService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSpecializationAsync([FromBody] SpecializationIncomingDto incomingDto)
+        {
+            var id = await _specializationsService.CreateAsync(incomingDto);
+            return CreatedAtRoute("GetSpecialization", new { id = id }, id);
+        }
+
+        [HttpGet("specialization/{id}", Name = "GetSpecialization")]
+        public async Task<IActionResult> GetSpecializationByIdAsync(Guid id)
+        {
+            var entity = await _specializationsService.GetByIdAsync(id);
+            return Ok(entity);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSpecializationsAsync()
+        {
+            var entities = await _specializationsService.GetAsync();
+            return Ok(entities);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSpecializationAsync(Guid id, [FromBody] SpecializationIncomingDto incomingDto)
+        {
+            await _specializationsService.UpdateAsync(id, incomingDto);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/status")]
+        public async Task<IActionResult> ChangeSpecializationStatusAsync(Guid id, [FromBody] string status)
+        {
+            await _specializationsService.ChangeStatusAsync(id, status);
+            return NoContent();
+        }
+    }
+}
