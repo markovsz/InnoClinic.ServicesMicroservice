0610c623b7e7625916158a0291583a0c4fec275a
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-29 01:15:28 +0300

    delete DTOs

--- InnoClinic.ServicesMicroservice/Application/DTOs/Outgoing/SpecializationMinOutgoingDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.DTOs.Outgoing;
-
-public class SpecializationMinOutgoingDto
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Status { get; set; }
-}
