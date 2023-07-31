59d49b4c1c2651e68062ca1955dd63861e33991e
markovsz <58207528+markovsz@users.noreply.github.com>
2023-08-01 00:02:28 +0300

    add AddServiceIncomingDtoValidator

---
+++ InnoClinic.ServicesMicroservice/Application/Validators/AddServiceIncomingDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Enums;
+using FluentValidation;
+using InnoClinic.SharedModels.DTOs.Services.Incoming;
+
+namespace Application.Validators;
+
+public class AddServiceIncomingDtoValidator : AbstractValidator<AddServiceIncomingDto>
+{
+	public AddServiceIncomingDtoValidator()
+	{
+		RuleFor(e => e.Name).NotEmpty();
+		RuleFor(e => e.Price).GreaterThan(0.0m);
+		RuleFor(e => e.Status).IsEnumName(typeof(Status));
+	}
+}
